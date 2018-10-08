4b2fc7bd7ea2c8959b33f3ff6b7b44a2ccc6d2e5
shimodatkh <shimdoatkh@nttdata.co.jp>
2018-10-09 01:26:03 +0900

    11.2.3.7. 例外ハンドリングの実装　まで

--- todo/src/main/java/todo/domain/service/todo/TodoServiceImpl.java
+++ todo/src/main/java/todo/domain/service/todo/TodoServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.terasoluna.gfw.common.exception.BusinessException;
 import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
-import org.terasoluna.gfw.common.message.ResultMessage;
 import org.terasoluna.gfw.common.message.ResultMessages;
 
 import todo.domain.model.Todo;
@@ -31,9 +30,7 @@ public class TodoServiceImpl implements TodoService {
         Todo todo = todoRepository.findOne(todoId);
         if (todo == null) {
             ResultMessages messages = ResultMessages.error();
-            messages.add(ResultMessage
-                    .fromText("[E404] The requested Todo is not found. (id="
-                            + todoId + ")"));
+            messages.add("E404", todoId);
             throw new ResourceNotFoundException(messages);
         }
         return todo;
@@ -50,9 +47,7 @@ public class TodoServiceImpl implements TodoService {
         long unfinishedCount = todoRepository.countByFinished(false);
         if (unfinishedCount >= MAX_UNFINISHED_COUNT) {
             ResultMessages messages = ResultMessages.error();
-            messages.add(ResultMessage
-                    .fromText("[E001] The count of un-finished Todo must not be over "
-                            + MAX_UNFINISHED_COUNT + "."));
+            messages.add("E001", MAX_UNFINISHED_COUNT);
             throw new BusinessException(messages);
         }
 
@@ -76,9 +71,7 @@ public class TodoServiceImpl implements TodoService {
         Todo todo = findOne(todoId);
         if (todo.isFinished()) {
             ResultMessages messages = ResultMessages.error();
-            messages.add(ResultMessage
-                    .fromText("[E002] The requested Todo is already finished. (id="
-                            + todoId + ")"));
+            messages.add("E002", todoId);
             throw new BusinessException(messages);
         }
         todo.setFinished(true);
